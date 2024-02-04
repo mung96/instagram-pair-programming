ec29c19a1d239f470de2f78504c98ab19b9dbe85
ChaMin <kusoft1997@gmail.com>
2024-02-04 17:08:09 +0900

    ui) Storyfeed 무지개 border 적용

--- js/storyfeed.js
+++ js/storyfeed.js
@@ -1,18 +1,14 @@
+let storyCheck=[false,false,false];
+const storyProfiles = document.quarySelectorAll(".storyfeed_item_profile");
 
 
-const storyProfile = document.quarySelector("storyfeed_item_profile")
-const Clicks = getAll('storyfeed_item_profile')
-
-
-
-function clickStory(){
-    const selectedStoryElement = storycolor[0];
-
-    if(!check){
-      storycolor.style.border = "2px solid rainbow";
+function storyClickHandler(i){
+    
+    if(!storyCheck){
+      storyProfiles[i].style.border = "2px solid rainbow";
     }
     else{
-      storycolor.style.border = "2px solid gray";
+      storyProfiles[i].style.border = "2px solid gray";
     }
     Clicks.forEach((profile) => {
         button.addEventListener('click', clicked);
@@ -32,14 +28,11 @@ function clickLike(){
     }
     else{
       heart.src="img/icon_RedHeart.png";
-      /*click 여러번으로 상태 바뀌지 않으니까 T/F check 필요 x */
     }
 }
 
 
-/*이걸 css랑 html에는 어떻게 적용할지?*/
 
 
 init();
 
-/*story 사진 클릭시 무지개색에서 회색으로 바뀌게끔*/
