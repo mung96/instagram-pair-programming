51d1cfecb2f8da7820a8e387286941a6a87dbf2f
ChaMin <kusoft1997@gmail.com>
2024-02-04 16:49:28 +0900

    feature) 이미지 슬라이드, 하드클릭 구현완료

--- js/index.js
+++ js/index.js
@@ -1,57 +1,56 @@
 // 이미지 슬라이드
-let imgIndex = 0;
-let position = 0;
-let check=false;
+let imgIndex=[0,0,0];
+let position = [0,0,0];
+let check=[false,false,false];
 const IMG_WIDTH = 470;
-const $btnPrev = document.querySelector(".btn-prev");
-const $btnNext = document.querySelector(".btn-next");
-const $slideImgs = document.querySelector(".postcard_slide");
-const heart=document.getElementById("heart");
+const btnPrev = document.querySelectorAll(".btn-prev");
+const btnNext = document.querySelectorAll(".btn-next");
+const slideImgs = document.querySelectorAll(".postcard_slide");
+const hearts=document.querySelectorAll(".postcard_bottom_icon_heart");
 
-
-
-let prev = function () {
-  if (imgIndex > 0) {
-    $btnNext.removeAttribute("disabled");
-    position += IMG_WIDTH;
-    $slideImgs.style.transform = `translateX(${position}px)`;
-    imgIndex = imgIndex - 1;
+function prev (i) {
+  if (imgIndex[i] > 0) {
+    btnNext[i].removeAttribute("disabled");
+    position[i] += IMG_WIDTH;
+    slideImgs[i].style.transform = `translateX(${position[i]}px)`;
+    imgIndex[i] = imgIndex[i] - 1;
   }
-  if (imgIndex == 0) {
-    $btnPrev.setAttribute("disabled", "true");
-  }
+  if (imgIndex[i] == 0) {
+    btnPrev[i].setAttribute("disabled", "true");
+  } 
 };
 
-let next = function () {
-  if (imgIndex < 3) {
-    $btnPrev.removeAttribute("disabled");
-    position -= IMG_WIDTH;
-    $slideImgs.style.transform = `translateX(${position}px)`;
-    $slideImgs.style.transition = "transform .5s ease-out";
-    imgIndex = imgIndex + 1;
+function next (i) { 
+  if (imgIndex[i] < 3) {
+    btnPrev[i].removeAttribute("disabled");
+    position[i] -= IMG_WIDTH;
+    slideImgs[i].style.transform = `translateX(${position[i]}px)`;
+    slideImgs[i].style.transition = "transform .5s ease-out";
+    imgIndex[i] = imgIndex[i] + 1;
   }
-  if (imgIndex == 2) {
-    $btnNext.setAttribute("disabled", "true");
-  }
+  if (imgIndex[i] == 2) {
+    btnNext[i].setAttribute("disabled", "true");
+} 
 };
 
 function init () {
-  $btnPrev.setAttribute("disabled", "true");
-  $btnPrev.addEventListener("click", prev);
-  $btnNext.addEventListener("click", next);
- 
+  for(let i=0;i<3;i++){
+    btnPrev[i].setAttribute("disabled", "true");
+    btnPrev[i].addEventListener("click", ()=>prev(i));
+    btnNext[i].addEventListener("click", ()=>next(i));
+    hearts[i].addEventListener("click",()=>heartClickHandler(i))
+  }
 };
-function clickLike(){
-  console.log("하트");
-  if(!check){
-    heart.src="img/icon_Heart.svg";
-    check=true;
+
+init();
+function heartClickHandler(i){
+  if(!check[i]){
+    hearts[i].src="img/icon_RedHeart.png";
+    check[i]=true;
   }
   else{
-    heart.src="img/icon_RedHeart.png";
-    check=false;
+    hearts[i].src="img/icon_Heart.svg";
+    check[i]=false;
   }
 
 }
-
-init();
